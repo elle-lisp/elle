dafbab2e46a187bf0a0308e8ff329a67ca43c160
Smooth Operator <github@andy.disruptek.com>
2026-03-06 10:23:42 -0500

    chore: consolidate examples, move logo to demos, clean test-modules

--- test-modules/test.lisp
+++
@@ -1,6 +0,0 @@
-# Simple test module for import-file tests
-# This file contains basic definitions for testing module loading
-
-(var test-var 42)
-(var test-string "hello")
-(var test-list (list 1 2 3))
